c3f1e63aa9e4d87022e339dd782eb8f20eb204b5
Avik Sarkar <140150423+csedebjit@users.noreply.github.com>
2025-11-06 17:48:04 +0530

    as

--- src/pages/App.js
+++ src/pages/App.js
@@ -6,7 +6,7 @@ import "./Housekeeper";
 import "./Plumber";
 import { useNavigate } from "react-router-dom";
 
-function App() {
+function  App() {
   const navigate = useNavigate();
   const houseKeeper = () => {
     navigate("/housekeeper");
